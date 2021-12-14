453851e1e2246951e1687b5591ac3e1f53c96164
Lucian Fratila <sirtitish@gmail.com>
2021-12-14 17:15:31 +0200

    v1

--- app.js
+++ app.js
@@ -6,15 +6,7 @@ const middleware = require('./AuthConfig/middleware');
 
 
 
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'front-end/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'front-end/build', 'index.html'));
-  });
-}
+
 
 
 
